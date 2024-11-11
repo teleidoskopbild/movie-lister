2dffc0e03cd461426bbb9e76f8ae23817347f6ea
teleidoskopbild <teleidoskopbild@gmail.com>
2024-11-11 23:12:09 +0100

    add fetch for tvshows and actors and display them on the pages

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -14,3 +14,30 @@ export async function fetchTrendingMovies() {
   }
   return response.json();
 }
+
+
+export async function fetchTrendingShows() {
+  const response = await fetch(`${BASE_URL}/trending/tv/week`, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch trending tv shows");
+  }
+  return response.json();
+}
+
+export async function fetchTrendingActors() {
+  const response = await fetch(`${BASE_URL}/trending/person/week`, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch trending actors");
+  }
+  return response.json();
+}
