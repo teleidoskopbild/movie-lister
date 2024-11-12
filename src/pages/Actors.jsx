a8e655de0a53c380635eacd06a40ba731ff29a7e
teleidoskopbild <teleidoskopbild@gmail.com>
2024-11-12 23:02:25 +0100

    style movies/shows/actors pages with tailwind and flex

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { fetchTrendingActors } from '../api/tmdbApi.js';
+import { useState, useEffect } from "react";
+import { fetchTrendingActors } from "../api/tmdbApi.js";
 
 function Actors() {
   const [actors, setActors] = useState([]);
@@ -13,8 +13,8 @@ function Actors() {
         setActors(data.results);
         setLoading(false);
       } catch (error) {
-        console.error("Error fetching actors!", error)
-        setError('Error fetching actors!');
+        console.error("Error fetching actors!", error);
+        setError("Error fetching actors!");
         setLoading(false);
       }
     };
@@ -30,9 +30,9 @@ function Actors() {
   }
 
   return (
-    <div>
-      <h1>Trending Actors</h1>
-      <div className="movie-list">
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Trending Actors</h1>
+      <div className="movie-list flex flex-wrap justify-center gap-8">
         {actors.map((actor) => (
           <div key={actor.id} className="movie-item">
             <img
@@ -40,7 +40,7 @@ function Actors() {
               alt={actor.name}
               className="movie-poster w-48 h-72 object-cover rounded-lg shadow-lg"
             />
-            <h2>{actor.name}</h2>
+            <h2 className="text-center">{actor.name}</h2>
           </div>
         ))}
       </div>
