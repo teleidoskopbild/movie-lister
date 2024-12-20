6af96b124976a69d78be00188877c614519f9d5c
teleidoskopbild <teleidoskopbild@gmail.com>
2024-12-20 15:55:22 +0100

    add github pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ export default function Footer() {
   return (
     <footer className="mx-12 mt-4">
       <p>&copy; 2024 Movie Lister. All rights reserved.</p>
+      <p></p>
     </footer>
   );
 }
